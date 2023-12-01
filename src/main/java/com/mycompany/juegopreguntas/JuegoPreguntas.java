29d9c23fdf015f7ef7c7069bd5837c51c2504ef4
maarkostosiic9 <mstosic@itpfp.com>
2023-12-01 11:26:34 +0100

    first commit

---
+++ src/main/java/com/mycompany/juegopreguntas/JuegoPreguntas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juegopreguntas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JuegoPreguntas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
